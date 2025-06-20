b78a14d609412e5160a944bb75022d0337c5dd30
felipeln <76855578+felipeln@users.noreply.github.com>
2025-06-20 14:51:13 -0300

    fix: fixing update.sh script

--- update.sh
+++ update.sh
@@ -1,15 +1,26 @@
 # update tmux config
-echo "updating 🔃  tmux config"
-cp -rf ~/.tmux/ ~/dotfiles/tmux
-cp -rf ~/.tmux.conf ~/dotfiles/tmux
+echo "updating 🔃 tmux config"
+
+rsync -avh --delete ~/.tmux/ ~/dotfiles/tmux/
+
+rsync -avh --delete ~/.tmux.conf ~/dotfiles/tmux/
+
 # update alacritty config
-echo "updating 🔃  alacritty config"
-cp -rf /mnt/c/users/felipe/appdata/roaming/alacritty ~/dotfiles/alacritty
+
+echo "updating 🔃 alacritty config"
+
+rsync -avh --delete /mnt/c/users/felipe/appdata/roaming/alacritty/ ~/dotfiles/alacritty/
+
 # update helix config
-echo "updating 🔃  helix config"
-cp -rf ~/.config/helix ~/dotfiles/
+
+echo "updating 🔃 helix config"
+
+rsync -avh --delete ~/.config/helix/ ~/dotfiles/helix/
+
 # update zsh config
-echo "updating 🔃  zsh config"
-cp -rf ~/.zshrc ~/dotfiles/zsh/
+
+echo "updating 🔃 zsh config"
+
+rsync -avh --delete ~/.zshrc ~/dotfiles/zsh/
 
 echo "setup updated ✅"
